5333b36570920b250a3c32d01ec0f5a388815eb5
Polina <94698638+orhunova-p@users.noreply.github.com>
2023-09-15 20:35:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,3 @@ board = Board(size, prob)
 screen_size = (550, 550)
 game = Game(board, screen_size)
 game.run()
-
-WIDTH = 30
-HEIGHT = 30
-# Sets the starting number of squares
-NSQUARES = 10
-# Sets the margin between each cell
-MARGIN = 5
-MENU_SIZE = 40
-LEFT_CLICK = 1
-RIGHT_CLICK = 3
-
-#self.grid = [[self.Cell(x, y) for x in range(NSQUARES)] for y in range(NSQUARES)]
